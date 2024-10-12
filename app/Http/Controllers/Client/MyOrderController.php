cbaaa35c44a449708ebebb716f5bfc35b4af7b72
doan2202 <trensky66@gmail.com>
2024-10-12 20:10:55 +0700

    feat:menu order status

--- app/Http/Controllers/Client/MyOrderController.php
+++ app/Http/Controllers/Client/MyOrderController.php
@@ -7,21 +7,26 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Services\Client\MyOrderService;
+use App\Services\UserService;
 
 class MyOrderController extends Controller
 {
     protected $myOrderService;
+    protected $userService;
 
-    public function __construct(MyOrderService $myOrderService)
+    public function __construct(MyOrderService $myOrderService, UserService $userService)
     {
         $this->myOrderService = $myOrderService;
+        $this->userService = $userService;
     }
 
-    public function myOrders()
+    public function myOrders(Request $request)
     {
-        $orders = $this->myOrderService->getOrder();
-
-        return view('client.my-account.my-order', compact('orders'));
+        $currentUser = $this->userService->getCurrentUser();
+        $status = $request->query('status');
+        $searchTerm = $request->input('search');
+        $orders = $this->myOrderService->getOrder($status, $searchTerm);
+        return view('client.my-account.my-order', compact('orders', 'currentUser', 'status'));
     }
     public function show($id)
     {
