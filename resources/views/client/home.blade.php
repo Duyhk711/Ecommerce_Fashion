b112f7d449c6ae434a952417e018004d638c3e3e
thanhdx <anhthanhfcbd@gmail.com>
2024-11-15 14:59:31 +0700

    sửa nút lưu voucher

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -64,21 +64,21 @@
         }
 
         /* .voucher-copy {
-            
-            color: #ffffff;
-            border: none;
-            padding: 4px 8px;
-            height: 30px;
-            width: 80px;
-            border-radius: 4px;
-            font-size: 12px;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-        } */
+                
+                color: #ffffff;
+                border: none;
+                padding: 4px 8px;
+                height: 30px;
+                width: 80px;
+                border-radius: 4px;
+                font-size: 12px;
+                cursor: pointer;
+                transition: background-color 0.3s ease;
+            } */
 
         /* .voucher-copy:hover {
-            background-color: #2f415d;
-        } */
+                background-color: #2f415d;
+            } */
 
         .xt {
             display: flex;
@@ -105,63 +105,40 @@
             max-width: 20ch;
         }
 
-        /* Nút Lưu */
-        .voucher-button-save {
-            background-color: #0084ff;
-            color: #ffffff;
-            border: none;
-            padding: 10px 16px;
-            border-radius: 8px;
-            cursor: pointer;
-            line-height: 1;
+        .voucher-button-save,
+        .voucher-button-saved,
+        .voucher-button-out-of-stock {
+            width: 100px;
             height: 40px;
-            display: flex;
-            text-align: center;
-            font-weight: bold;
-            transition: background-color 0.3s ease;
-            line-height: 1;
-            height: 40px;
-            display: flex;
+            display: inline-flex;
             align-items: center;
             justify-content: center;
+            font-weight: bold;
+        }
+
+        .voucher-button-save {
+            background-color: #0d6efd;
+            color: #ffffff;
+            transition: background-color 0.3s ease;
         }
 
         .voucher-button-save:hover {
-            background-color: #e64a19;
+            background-color: #e0e7ff;
+            color: #4c6ef5;
         }
 
-        /* Nút Đã Lưu */
         .voucher-button-saved {
-            background-color: none;
-            color: #000000;
-            border: 1px solid #0084ff;
-            padding: 10px 16px;
-            border-radius: 8px;
+            background-color: #e0e7ff;
+            color: #4c6ef5;
+
             cursor: not-allowed;
-            text-align: center;
-            font-weight: bold;
-            line-height: 1;
-            height: 40px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
         }
 
-        /* Nút Đã Hết */
         .voucher-button-out-of-stock {
-            background-color: #bdbdbd;
+            background-color: #cccccc;
             color: #ffffff;
-            border: none;
-            padding: 10px 16px;
-            border-radius: 8px;
+
             cursor: not-allowed;
-            text-align: center;
-            font-weight: bold;
-            line-height: 1;
-            height: 40px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
         }
     </style>
     <section class="slideshow slideshow-wrapper">
